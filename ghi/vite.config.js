9dc82b19a9ba72bcb6b64fc624b55d4bc51c9650
hamim442 <hamim442@icloud.com>
2024-10-05 16:16:36 -0700

    docker running

---
+++ ghi/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // This should match the output path in your Dockerfile
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
